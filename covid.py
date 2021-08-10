5b952f87b22d727b5a672a93a2b9e66c1a01019c
Khalid Al-Walid bin Mohd Aris <khalidalwalid.mohdaris@gmail.com>
2021-08-10 17:05:29 +0800

    change data source

--- covid.py
+++ covid.py
@@ -1,3 +1,4 @@
+import datetime
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
@@ -8,11 +9,11 @@ from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 
 # Import data 
-# df = pd.read_csv("https://raw.githubusercontent.com/khalidal-walid/covid/main/covid-19%20malaysia.csv")
-# df2 = pd.read_csv("https://raw.githubusercontent.com/khalidal-walid/covid/main/covid-19%20state.csv")
-
-df = pd.read_csv("/Users/tengku/Project/Covid/covid-19 malaysia.csv")
-df2 = pd.read_csv("/Users/tengku/Project/Covid/covid-19 state.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/epidemic/cases_malaysia.csv")
+df2 = pd.read_csv("https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/epidemic/cases_state.csv")
+df3 = pd.read_csv("https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/epidemic/deaths_malaysia.csv")
+df4 = pd.read_csv("https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/epidemic/hospital.csv")
+df5 = pd.read_csv("https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/epidemic/tests_malaysia.csv")
 
 # Creates a list of dictionaries
 def get_options(list_state):
@@ -21,6 +22,21 @@ def get_options(list_state):
         state_list.append({'label': i, 'value': i})
 
     return state_list
+
+
+# percentage calculation
+death_percentage = df3['deaths_new'].sum(axis=0) / df['cases_new'].sum(axis=0) * 100
+
+discharge_percentage = df4['discharged_total'].sum(axis=0) / df['cases_new'].sum(axis=0) * 100
+
+rtk_percentage = df['cases_new'].iloc[-1] / df5['rtk-ag'].iloc[-1] * 100
+
+pcr_percentage = df['cases_new'].iloc[-1] / df5['pcr'].iloc[-1] * 100
+
+
+# format date
+df['date'] = pd.to_datetime(df['date']).dt.strftime('%d %b %Y')
+
 
 # Initialise the app
 app = dash.Dash(
@@ -41,21 +57,21 @@ app.layout = html.Div([
 
   html.Div([
     html.Div([
-        html.H1(children='Jumlah kes keseluruhan'),
-        html.H2(f"{df['total cases'].iloc[-1]:,.0f}"),
-        html.P(f"Kes baharu: {df['new cases'].iloc[-1]}")
+        html.H1(children='Kes baharu'),
+        html.H2(f"{df['cases_new'].iloc[-1]}"),
+        html.P(f"Jumlah kes: {df['cases_new'].sum(axis=0)}")
       ], className= 'info'),
 
     html.Div([
-        html.H1(children='Jumlah kes sembuh'),
-        html.H2(f"{df['total discharged'].iloc[-1]:,.0f}"),
-        html.P(f"Kes sembuh baharu: {df['new discharged'].iloc[-1]}")
+        html.H1(children='Kes sembuh baharu'),
+        html.H2(f"{df4['discharged_total'].groupby(df4['date']).sum().iloc[-1]}"),
+        html.P(f"Jumlah kes sembuh: {df4['discharged_total'].sum(axis=0)}"),
     ], className= 'info'),
 
     html.Div([
-        html.H1(children='Jumlah kes kematian'),
-        html.H2(f"{df['total death'].iloc[-1]:,.0f}"),
-        html.P(f"Kes kematian baharu: {df['new death'].iloc[-1]}")
+        html.H1(children='Kes kematian baharu'),
+        html.H2(f"{df3['deaths_new'].iloc[-1]}"),
+        html.P(f"Jumlah kes kematian: {df3['deaths_new'].sum(axis=0)}")
     ], className= 'info')
 
   ], className= 'main-info'),
@@ -64,22 +80,21 @@ app.layout = html.Div([
     html.Div([
       html.H1(children='Statistik semasa'),
 
-      html.P(f"Peratus sudah menerima dos pertama: {df['% 1st dose'].iloc[-1]}"),
-      dbc.Progress(value=11.68, max=100, striped=True, color="success", style={"height": "40px", 'margin': '1em'}),
+      html.P(f"Peratus sembuh: {round(discharge_percentage, 2)}%"),
+      dbc.Progress(value=discharge_percentage, max=100, striped=True, style={"height": "40px", 'margin': '1em'}),
 
-      html.P(f"Peratus sudah menerima dos kedua: {df['% 2nd dose'].iloc[-1]}"),
-      # dbc.Progress(children=["0.06%"], value=1, max=100, striped=True, color="success", style={"height": "40px", 'margin': '1em'}), 
-      dbc.Progress(value=7.23, max=100, striped=True, color="success", style={"height": "40px", 'margin': '1em'}), 
+      html.P(f"Kes dikesan dari ujian RTK-AG: {round(rtk_percentage, 2)}%"),
+      dbc.Progress(value=pcr_percentage, max=100, striped=True, style={"height": "40px", 'margin': '1em'}), 
 
-      html.P(f"Peratus sembuh: {df['% discharged'].iloc[-1]}"),
-      dbc.Progress(value=90.63, max=100, striped=True, color="success", style={"height": "40px", 'margin': '1em'}),
+      html.P(f"Kes dikesan dari ujian PCR: {round(pcr_percentage, 2)}%"),
+      dbc.Progress(value=pcr_percentage, max=100, striped=True, style={"height": "40px", 'margin': '1em'}), 
 
-      html.P(f"Peratus kematian: {df['% death'].iloc[-1]}"),
-      dbc.Progress(value=0.39, max=100, striped=True, color="danger", style={"height": "40px", 'margin': '1em'})      
+      html.P(f"Peratus kematian: {round(death_percentage,2)}%"),
+      dbc.Progress(value=death_percentage, max=100, striped=True, style={"height": "40px", 'margin': '1em'})      
     ], className= 'graph-1'),
 
     html.Div([
-        html.H1(children='Kes mengikut negeri (Mei 2021)'),
+        html.H1(children='Kes mengikut negeri'),
               html.Div(
                 className='div-for-dropdown',
                 children=[
@@ -114,7 +129,7 @@ def update_line_chart(state_selection):
   figure = px.line(
     data_frame = dff,
     x = 'date',
-    y ='total cases'
+    y ='cases_new'
   )
 
   figure.update_layout(
@@ -122,11 +137,11 @@ def update_line_chart(state_selection):
     plot_bgcolor= 'rgba(0, 0, 0, 0)',
     paper_bgcolor= '#1B233F',
     xaxis=dict(linecolor='gray'),
-    # yaxis=dict(linecolor='gray'),
+    yaxis=dict(linecolor='gray'),
     font_color='lightgray'
   )
 
-  figure.update_xaxes(title_text='Tarikh',showticklabels=False, showgrid=False, showline=True)
+  figure.update_xaxes(title_text='Tarikh',showticklabels=False, showgrid=False)
   figure.update_yaxes(title_text='Kes baharu', showticklabels=False, showgrid=False)
 
   return figure
